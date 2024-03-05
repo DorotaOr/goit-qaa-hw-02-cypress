b87630ac3e6aa8c6415ed8f7c6f83e85722bf9e3
DorotaOr <dorota.orr@gmail.com>
2024-03-05 16:56:59 +0100

    after correction

--- cypress/e2e/pages/HomePage.js
+++ cypress/e2e/pages/HomePage.js
@@ -1,20 +1,25 @@
 class homePage {
+  constructor() {
     this.courseHomePageWidget = 'url "include", "/homepage"';
     this.openMenu = "#open-navigation-menu-mobile";
     this.nextButton = "button.next-bve2vl.e1phyiqy2";
-    
-}
+  }
 
-goToCourseHomePage() {
+  goToCourseHomePage() {
     cy.get(this.courseHomePageWidget);
-}
+  }
 
-goToMenu() {
+  goToMenu() {
     cy.get(this.openMenu);
+  }
+
+  proceedToNext() {
+    cy.get(this.nextButton)
+      .contains("Log out")
+      .should("exist")
+      .should("be.visible")
+      .click();
+  }
 }
 
-proceedToNext() {
-    cy.get(this.nextButton).contains("Log out").should("exist").should("be.visible").click();
-    }
-
-    export default new homePage();
+export default new homePage();
