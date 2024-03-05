a302b81e5db6d6d0067728453f9d674b88db1dfa
DorotaOr <dorota.orr@gmail.com>
2024-03-05 16:49:11 +0100

    commit after finish tests writing

---
+++ cypress/e2e/LoginTests.js
@@ -0,0 +1,22 @@
+import loginPage from "./pages/Login";
+import homePage from "./pages/HomePage";
+
+describe("User can visit GoIT page", () => {
+  beforeEach("Go to page", () => {
+    loginPage.visit();
+  });
+});
+
+it("First log in and log out", () => {
+  loginPage.loginUser("user888@gmail.com", "1234567890");
+  homePage.goToCourseHomePage();
+  homePage.goToMenu();
+  homePage.proceedToNext();
+});
+
+it("Second log in and log out", () => {
+  loginPage.loginUser("testowyqa@qa.team", "QA!automation-1");
+  homePage.goToCourseHomePage();
+  homePage.goToMenu();
+  homePage.proceedToNext();
+});
---
+++ cypress/e2e/pages/HomePage.js
@@ -0,0 +1,20 @@
+class homePage {
+    this.courseHomePageWidget = 'url "include", "/homepage"';
+    this.openMenu = "#open-navigation-menu-mobile";
+    this.nextButton = "button.next-bve2vl.e1phyiqy2";
+    
+}
+
+goToCourseHomePage() {
+    cy.get(this.courseHomePageWidget);
+}
+
+goToMenu() {
+    cy.get(this.openMenu);
+}
+
+proceedToNext() {
+    cy.get(this.nextButton).contains("Log out").should("exist").should("be.visible").click();
+    }
+
+    export default new homePage();
