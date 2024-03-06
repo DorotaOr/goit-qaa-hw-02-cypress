02faf5eb8d7e90a178c5b1c3bcf3d30d67a9027b
DorotaOr <dorota.orr@gmail.com>
2024-03-06 16:52:07 +0100

    after testing

--- cypress/pages/Login.cy.js
+++ cypress/pages/Login.cy.js
@@ -1,17 +1,21 @@
 class loginPage {
   constructor() {
     this.url = "https://www.edu.goit.global/account/login";
-    this.emailInput = 'input[name="email"]';
+    this.emailInput = 'input[class="next-v891b4.eteu1jj2"]';
     this.passwordInput = 'input[type="password"]';
     this.loginButton = 'button[type="submit"]';
   }
+
+  // '#user_email' 'input[name="email"]' *[@id="user_email"] ".next-v891b4 eteu1jj2"
 
   visit() {
     cy.visit(this.url);
   }
 
   loginUser(email, password) {
-    cy.get(this.emailInput).type(email);
+    cy.get(this.emailInput, { timeout: 10000 })
+      //.should("be.visible")
+      .type(email);
     cy.get(this.passwordInput).type(password);
     cy.get(this.loginButton).click();
   }
